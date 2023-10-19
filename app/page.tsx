91f17ffd7f5569c067b5bcf6cb66263580cbef6f
devopsdbroe <dbroe1995@gmail.com>
2023-10-19 13:59:43 -0400

    Added profile pic to Intro

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import Intro from "@/components/Intro";
 import Image from "next/image";
 
 export default function Home() {
-	return <main className=""></main>;
+	return (
+		<main className="">
+			<Intro />
+		</main>
+	);
 }
