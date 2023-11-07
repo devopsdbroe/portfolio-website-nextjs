e0f3c21ece75f07d9b68f6529c7eb350d46fb211
devopsdbroe <dbroe1995@gmail.com>
2023-11-06 22:07:02 -0500

    Implemented Dark Mode feature

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import React from "react";
 import SectionHeading from "./SectionHeading";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
@@ -30,7 +29,7 @@ const Contact = () => {
 			}}
 		>
 			<SectionHeading>Contact me</SectionHeading>
-			<p className="text-gray-700 -mt-6">
+			<p className="text-gray-700 -mt-6 dark:text-white/80">
 				Please contact me directly at{" "}
 				<a className="underline" href="mailto:dbroe@gmail.com">
 					dbroe1995@gmail.com
@@ -39,7 +38,7 @@ const Contact = () => {
 			</p>
 
 			<form
-				className="mt-10 flex flex-col"
+				className="mt-10 flex flex-col dark:text-black"
 				action={async (formData) => {
 					const { data, error } = await sendEmail(formData);
 
@@ -52,7 +51,7 @@ const Contact = () => {
 				}}
 			>
 				<input
-					className="h-14 px-4 rounded-lg borderBlack"
+					className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
 					name="senderEmail"
 					type="email"
 					required
@@ -60,7 +59,7 @@ const Contact = () => {
 					placeholder="Your email"
 				/>
 				<textarea
-					className="h-52 my-3 rounded-lg borderBlack p-4"
+					className="h-52 my-3 rounded-lg borderBlack p-4  dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
 					name="message"
 					required
 					maxLength={5000}
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Footer = () => {
 	return (
 		<footer className="mb-10 px-4 text-center text-gray-500">
