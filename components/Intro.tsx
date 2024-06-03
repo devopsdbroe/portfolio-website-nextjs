d4c7e9cb7e00f82787e47e2283021e4ccf0576d4
Daniel Broe <dbroe1995@gmail.com>
2024-06-03 16:03:58 -0400

    Update GitHub link in Intro.tsx

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -102,7 +102,7 @@ const Intro = () => {
 				</a>
 				<a
 					className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-					href="https://github.com/devopsdbroe?tab=repositories"
+					href="https://github.com/devopsdbroe"
 					target="_blank"
 				>
 					<BsGithub />
